b5f6d90e1a4b02a330654d1d910cea6edbea50f5
𝕃𝕦𝕔𝕒𝕤 <lucas.skyzoc@gmail.com>
2025-08-29 22:38:24 -0300

    feat: implement projects system with filters - working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Hero from './sections/Hero';
 import About from './sections/About';
 import Services from './sections/Services';
+import Projects from './sections/Projects';
 import Contact from './sections/Contact';
 import useSmoothScroll from './hooks/useSmoothScroll';
 
@@ -15,6 +16,7 @@ function App() {
         <Hero />
         <About />
         <Services />
+        <Projects />
         <Contact />
       </main>
     </div>
